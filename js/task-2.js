51098863ad0b5a3620f0d10d9321d229c08e9ace
Cyberons <platon.pustovoyt@gmail.com>
2024-01-03 12:49:39 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,11 @@
 function formatMessage(message, maxLength) {
-    return message.slice(0, maxLength)
+     if (message.length > maxLength) {
+        return message.slice(0, maxLength) + "...";
+    } else {
+        return message;
+    }
 }
+
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
